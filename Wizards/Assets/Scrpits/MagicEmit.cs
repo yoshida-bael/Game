83e7500bdcfb7db7063b4ffeed7db29e59bb90b2
吉田育磨 <>
2019-09-23 18:18:51 +0900

    add magic attack

---
+++ Wizards/Assets/Scrpits/MagicEmit.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicEmit : MonoBehaviour
+{
+    public GameObject MagicPrefab;
+	public float shotSpeed;
+
+	void Update () {
+		if (Input.GetKey (KeyCode.Mouse0)) {
+			GameObject Magic = (GameObject)Instantiate (MagicPrefab, transform.position, Quaternion.Euler (transform.parent.eulerAngles.x, transform.parent.eulerAngles.y, 0));
+			Rigidbody MagicRb = Magic.GetComponent<Rigidbody> ();
+			MagicRb.AddForce (transform.forward * shotSpeed);
+        }
+	}
+}
